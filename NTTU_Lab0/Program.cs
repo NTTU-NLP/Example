609e2bab53a9df2308922a5f5324931711d05a8a
Hong-Jie Dai <hongjiedai@gmail.com>
2017-02-17 18:55:16 +0800

    加入 lab0

---
+++ NTTU_Lab0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTTU_Lab0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(@"0001_gs.xml"))
+            {
+                while (sr.Peek() != -1)
+                {
+                    string line = sr.ReadLine();
+                    foreach (var ch in line)
+                    {
+                        Console.Write(ch);
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
